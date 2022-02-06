ec81d299f4c473fc6dbb42df0234853c731ab07f
renan.uemura <renan.uemura@logicsp.com.br>
2022-02-06 20:28:53 -0300

    first commit

---
+++ src/main/java/uemura/java_spring_boot_demo/JavaSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package uemura.java_spring_boot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uemura/java_spring_boot_demo/JavaSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uemura.java_spring_boot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
