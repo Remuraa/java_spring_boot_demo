51bc7a6ed705c1cf4bcdbf073dfde109dd4fe1ec
renan.uemura <renan.uemura@logicsp.com.br>
2022-02-07 22:23:18 -0300

    Fix connection h2

--- src/main/java/uemura/java_spring_boot_demo/domais/SalesEntity.java
+++ src/main/java/uemura/java_spring_boot_demo/domais/SalesEntity.java
@@ -1,12 +1,10 @@
 package uemura.java_spring_boot_demo.domais;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
+@Table(name = "sales")
 public class SalesEntity implements Serializable {
 
     @Id
