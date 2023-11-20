6bdf6e73530a9c3f5c585f1bbc16372314f39216
renan.uemura <renan.uemura@logicsp.com.br>
2023-11-20 15:39:55 -0300

    Add version

--- src/main/java/uemura/java_spring_boot_demo/JavaSpringBootDemoApplication.java
+++ src/main/java/uemura/java_spring_boot_demo/JavaSpringBootDemoApplication.java
@@ -1,13 +1,22 @@
 package uemura.java_spring_boot_demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class JavaSpringBootDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaSpringBootDemoApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(JavaSpringBootDemoApplication.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(JavaSpringBootDemoApplication.class, args);
+        ConfigurableEnvironment environment = run.getEnvironment();
+
+        log.info("\nJava Spring Boot Demo version: {}", environment.getProperty("info.version"));
+    }
 
 }
